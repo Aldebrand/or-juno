f1730064348a49819d6297095440b1c3009b18c3
Or Chen <archybald9@gmail.com>
2019-03-05 21:20:39 +0200

    update

--- QueryExecutor/src/main/java/ObjectParser.java
+++
@@ -1,4 +0,0 @@
-public interface ObjectParser<T> {
-
-    Object parse(T object);
-}
--- QueryExecutor/src/main/java/QueryExecutorBase.java
+++
@@ -1,9 +0,0 @@
-public abstract class QueryExecutorBase<T> {
-    protected String connString;
-
-    public QueryExecutorBase(String connString){
-        this.connString = connString;
-    }
-
-    abstract T executeQuery(Query query);
-}
