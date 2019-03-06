c318e5c7a35a63deeba735a459b9bed18841921b
Or Chen <archybald9@gmail.com>
2019-03-06 11:03:53 +0200

    removed ran's email password

--- data_engineer_exercise/tasks/xref_report.sh
+++ data_engineer_exercise/tasks/xref_report.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 curl --url 'smtps://smtp.gmail.com:465' --ssl-reqd \
   --mail-from 'runtibi@gmail.com' --mail-rcpt 'archybald9@gmail.com' \
-  --upload-file D:\\Workspace\\Java\\QueryExecutor\\test.csv --user 'runtibi@gmail.com:tr281089' --insecure
+  --upload-file D:\\Workspace\\Java\\QueryExecutor\\test.csv --user 'runtibi@gmail.com:' --insecure
